1f7711678cb6e9b631a96d22504dc727ecb8cc3a
franlops <franlopbri@gmail.com>
2022-09-28 10:51:46 -0500

    Implementando funcion y limpia consola

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import os
+
 def suma(num1, num2):
 	return num1 + num2
 
@@ -8,24 +10,36 @@ def multiply(num1, num2):
 	return num1 * num2
 
 def divide(num1, num2):
+	if num2 == 0:
+		num2 = 1
 	return num1 / num2
+
+def return_values():
+	num1 = int(input("Ingresa un numero: "))
+	num2 = int(input("Ingresa otro numero: "))
+	return [num1, num2]
 
 if __name__ == '__main__':
 	message = f"Calculadora: \n Elige una opción:\n1 - suma\n2 - resta\n3 - multiplicacion\n4 - division\n5 - Salir\n"
 	while True:
+		os.system('clear')
 		opcion = int(input(message))
 		# comparar cada una de las opciones y llamar a la funcion que corresponda
 		if opcion == 1:
-			resultado_suma = suma(23, 54)
+			numeros = return_values()
+			resultado_suma = suma(numeros[0], numeros[1])
 			print("El resultado de la suma es", resultado_suma)
 		elif opcion == 2:
-			resultado_resta = resta(23, 54)
+			numeros = return_values()
+			resultado_resta = resta(numeros[0], numeros[1])
 			print("El resultado de la resta es", resultado_resta)
 		if opcion == 3:
-			resultado_multi = multiply(23, 54)
+			numeros = return_values()
+			resultado_multi = multiply(numeros[0], numeros[1])
 			print("El resultado de la multiplicación es", resultado_multi)
 		if opcion == 4:
-			resultado_div = divide(23, 54)
+			numeros = return_values()
+			resultado_div = divide(numeros[0], numeros[1])
 			print("El resultado de la división es", resultado_div)
 		elif opcion == 5:
 			print("Bye!")
