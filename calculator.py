75c9b697995db13123ec2ff5e147dbc5d4193d5f
franlops <franlopbri@gmail.com>
2022-09-28 10:17:14 -0500

    Implementa funcion pide numeros

--- calculator.py
+++ calculator.py
@@ -1,34 +1,48 @@
 def suma(num1, num2):
-	return num1 + num2
+    return num1 + num2
+
 
 def resta(num1, num2):
-	return num1 - num2
+    return num1 - num2
+
 
 def multiply(num1, num2):
-	return num1 * num2
+    return num1 * num2
+
 
 def divide(num1, num2):
-	return num1 / num2
+    return num1 / num2
+
+
+def return_values():
+    num1 = int(input("Ingresa un número: "))
+    num2 = int(input("Ingresa otro número: "))
+    return [num1, num2]
+
 
 if __name__ == '__main__':
-	message = f"Calculadora: \n Elige una opción:\n1 - suma\n2 - resta\n3 - multiplicacion\n4 - division\n5 - Salir\n"
-	while True:
-		opcion = int(input(message))
-		# comparar cada una de las opciones y llamar a la funcion que corresponda
-		if opcion == 1:
-			resultado_suma = suma(23, 54)
-			print("El resultado de la suma es", resultado_suma)
-		elif opcion == 2:
-			resultado_resta = resta(23, 54)
-			print("El resultado de la resta es", resultado_resta)
-		if opcion == 3:
-			resultado_multi = multiply(23, 54)
-			print("El resultado de la multiplicación es", resultado_multi)
-		if opcion == 4:
-			resultado_div = divide(23, 54)
-			print("El resultado de la división es", resultado_div)
-		elif opcion == 5:
-			print("Bye!")
-			break
-		else:
-			print("Opción Incorrecta")
+    message = f"Calculadora: \n Elige una opción:\n1 - suma\n2 - resta\n3 - multiplicacion\n4 - division\n5 - Salir\n"
+    while True:
+        opcion = int(input(message))
+        # comparar cada una de las opciones y llamar a la funcion que corresponda
+        if opcion == 1:
+            numeros = return_values()
+            resultado_suma = suma(numeros[0], numeros[1])
+            print("El resultado de la suma es", resultado_suma)
+        elif opcion == 2:
+            numeros = return_values()
+            resultado_resta = resta(numeros[0], numeros[1])
+            print("El resultado de la resta es", resultado_resta)
+        elif opcion == 3:
+            numeros = return_values()
+            resultado_multi = multiply(numeros[0], numeros[1])
+            print("El resultado de la multiplicación es", resultado_multi)
+        elif opcion == 4:
+            numeros = return_values()
+            resultado_div = divide(numeros[0], numeros[1])
+            print("El resultado de la división es", resultado_div)
+        elif opcion == 5:
+            print("Bye!")
+            break
+        else:
+            print("Opción Incorrecta")
